336ef2d33b6052a2cf6d34605caa984330554e57
Alexander Neo <AlexanderNeo427@gmail.com>
2025-04-07 12:46:25 +0800

    Bootstrapped ExpressjS project with necessary dependencies
    * Typescript support
    * "nodemon" and "ts-node" for hot-reloading
    * Compose file to setup PostgreSQL database
    * "ts-jest" and "supertest" for tests
    * PassportJS for authentication (+local strategy)
    * Misc (dotenv, bcrypt, prettier....)

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express"
+import dotenv from 'dotenv'
+import path from 'path'
+
+process.env.NODE_ENV = "dev"
+const envPath = path.resolve(
+    __dirname,
+    `../.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`
+);
+dotenv.config({ path: envPath })
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/", async (req: Request, res: Response) => {
+    const data = req.body
+    res.status(200).json({ "Here's the data we received: ": data })
+})
+
+const APP_PORT = process.env.PORT || 3000
+app.listen(APP_PORT, async () => {
+    console.log("Server listening on port", APP_PORT);
+})
